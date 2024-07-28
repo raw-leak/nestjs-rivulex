2e4db38de8c025d71c86d26eae649253148046b5
Mykhaylo Gusak <michaelxxi@outlook.es>
2024-07-28 10:28:03 +0200

    feat: :sparkles: exported new decorator

--- lib/index.ts
+++ lib/index.ts
@@ -6,3 +6,4 @@ export { RivulexTransport } from "./rivulex.transport"
 // decorators
 export { Action } from "./decorators/action.decorator"
 export { Stream } from "./decorators/stream.decorator"
+export { StreamAction } from "./decorators/stream-action.decorator"
