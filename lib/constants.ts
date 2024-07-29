bb95a3c7a537f28d71d6e9cbd3f2a3c7ac07d981
Mykhaylo Gusak <michaelxxi@outlook.es>
2024-07-29 12:57:42 +0200

    feat: :sparkles: renamed constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
-export const STREAM_METADATA_KEY = 'stream_name';
-export const ACTION_METADATA_KEY = 'action_name';
+export const STREAM_METADATA_KEY = 'rivulex:stream';
+export const ACTION_METADATA_KEY = 'rivulex:action';
